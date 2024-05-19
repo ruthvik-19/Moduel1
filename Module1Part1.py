4e05a1e83dd5de0563a5b32eaa1799b90dbe2c99
Ruthvik Nunna <ruthvik.nunna@gmail.com>
2024-05-19 11:47:49 -0500

    initial commit

---
+++ Module1Part1.py
@@ -0,0 +1,13 @@
+
+#User input converted to integer type
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+
+#calculate sum and difference
+sum = num1 + num2
+diff = num1 - num2
+
+# formatted print statement
+print("The sum of {} and {} is {}".format(num1, num2, sum))
+print("The difference of {} and {} is {}".format(num1, num2, diff))
+
---
+++ Module1Part2.py
@@ -0,0 +1,13 @@
+
+#User input converted from string to integer
+num1 = int(input("Enter the first number "))
+num2 = int(input("Enter the second number "))
+
+#variable that holds the value of the product of num1 and num2
+mult = num1 * num2
+#variable that holds the value of the quotient of num1 and num2
+div = num1 / num2
+
+#formatted print statment
+print("The product of {} and {} is {}".format(num1, num2, mult))
+print("The quotient of {} and {} is {}".format(num1, num2, div))
